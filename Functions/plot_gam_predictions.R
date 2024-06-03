598bdc1ba286e3839ecfd951d2ea4ff5d2c9812f
adupaix <amael.dupaix@ens-lyon.fr>
2024-06-03 18:53:44 +0200

    functions for new stat analysis

---
+++ Functions/plot_gam_predictions.R
@@ -0,0 +1,57 @@
+plot.gam.prediction <- function(data, my_gam,
+                                vars, var_to_predict,
+                                xlabel = "Xi",
+                                ylabel = "Y",
+                                lims.y = c(NA,NA),
+                                transformation = T,
+                                trans.back = identity,
+                                l = 200){
+  df <- data.frame(seq(min(data[,var_to_predict]),max(data[,var_to_predict]), length.out = l))
+  names(df) <- var_to_predict
+  other_vars <- vars[!vars %in% var_to_predict]
+  for (i in 1:length(other_vars)){
+    nm <- names(df)
+    df <- dplyr::bind_cols(df,
+                           new = rep(median(data[,other_vars[i]]),
+                                     l))
+    names(df) <- c(nm, other_vars[i])
+  }
+  p <- mgcv::predict.gam(my_gam, df, type = "link", se.fit = TRUE)
+  
+  
+  if (transformation){
+    p$fit <- trans.back(p$fit)
+    p$se.fit <- trans.back(p$se.fit)
+  }
+  upr <- p$fit + (p$se.fit)
+  lwr <- p$fit - (p$se.fit)
+  
+  g <-
+    ggplot()+
+    geom_line(aes(x = df[,var_to_predict],
+                  y = p$fit))+
+    geom_line(aes(x = df[,var_to_predict],
+                  y = upr),
+              linetype = "dotted")+
+    geom_line(aes(x = df[,var_to_predict],
+                  y = lwr),
+              linetype = "dotted")+
+    scale_y_continuous(limits = lims.y)+
+    xlab(xlabel)+ ylab(ylabel)+
+    theme(panel.background = element_rect(fill = "white",
+                                          color = "black"))  
+  
+  xhist <- 
+    axis_canvas(g, axis = "x") + 
+    geom_histogram(aes(x = data[,var_to_predict],
+                       y = after_stat(count / max(count))),
+                   bins = 100,
+                   fill = 'white',
+                   color = "black")
+  return(
+    g %>%
+      insert_xaxis_grob(xhist, grid::unit(0.5, "in"), position = "top") %>%
+      ggdraw()
+  )
+  
+}
