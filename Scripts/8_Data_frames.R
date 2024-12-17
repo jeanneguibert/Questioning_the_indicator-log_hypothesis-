1ce6c4705b9b7b8dcf54ac1ef52b5c4c3505da0a
adupaix <amael.dupaix@ens-lyon.fr>
2024-12-17 18:21:24 +0100

    adapt to temp res choice

--- Scripts/8_Data_frames.R
+++ Scripts/8_Data_frames.R
@@ -12,24 +12,33 @@ dfFSLE<-read.csv(file.path(PATH_OUTPUT, "FSLE_mean.csv"), header = T)
 dfSSCI<-read.csv(file.path(PATH_OUTPUT, "SSCI_mean.csv"), header = T)
 dfMN<-read.csv(file.path(PATH_OUTPUT, "MN_epi_mean.csv"), header = T)
 
-colnames(dfMN)[which(colnames(dfMN) == "micronec_epi")] <- "MNmean"
-colnames(dfMN)[which(colnames(dfMN) == "micronec_episd")] <- "MNsd"
+colnames(dfMN)[which(colnames(dfMN) == "micronec_epi_mean")] <- "MNmean"
+colnames(dfMN)[which(colnames(dfMN) == "micronec_epi_sd")] <- "MNsd"
 
 ###############Merge 
-df_list<-list(dfNLOG,dfChla,dfSST,dfSLA,dfFSLE,dfSSCI,dfMN)  
-dftot<-Reduce(function(x, y) merge(x, y, by=c("lat_grid", "lon_grid","year", "month")), df_list)  
+df_list<-list(dfNLOG,dfChla,dfSST,dfSLA,dfFSLE,dfSSCI,dfMN)
+if (NLOG_DATA_SOURCE == 'sim'){
+  merging_cols <- c("lat_grid", "lon_grid","time")
+} else if (NLOG_DATA_SOURCE == 'obs'){
+  merging_cols <- c("lat_grid", "lon_grid","year", "month")
+}
+dftot<-Reduce(function(x, y) merge(x, y, by=merging_cols), df_list)  
 summary(dftot)
 
 #filter to keep only years of interest
 #' and only area of interest (area_limits = c(xmin = 39, xmax = 90,
 #'                                            ymin = -20, ymax = 20))
 dftot %>%
+  dplyr::mutate(year = as.numeric(gsub('-.*', '', time))) %>%
   dplyr::filter(year %in% YEARS,
                 lon_grid >= AREA_LIMITS['xmin'] & lon_grid <= AREA_LIMITS['xmax'],
-                lat_grid >= AREA_LIMITS['ymin'] & lat_grid <= AREA_LIMITS['ymax']) -> dftot
+                lat_grid >= AREA_LIMITS['ymin'] & lat_grid <= AREA_LIMITS['ymax']) %>%
+  tidyr::drop_na() -> dftot
 
 #ajout Zone
 dftot$Zone <- as.factor(ifelse(dftot$lat_grid<(-10) & dftot$lon_grid<=(50),"MOZ","WIO"))
+dftot$Zone <- as.factor(ifelse(dftot$lon_grid<=50 & dftot$lon_grid>47 & dftot$lat_grid<(-15),
+                               'WIO', as.character(dftot$Zone)))
 
 #ajout Season
 if (timeresolution == 'month'){
@@ -59,25 +68,29 @@ write.csv(dftot, file.path(PATH_OUTPUT,"NLOG_VE.csv"), row.names = F)
 
 NLOG_VE<-read.csv(file.path(PATH_OUTPUT,"NLOG_VE.csv"), header = T)
 
-NLOG_VE_sup_zero <- NLOG_VE[NLOG_VE$NumNLOG>0,]
-#Moz <10S
-NLOG_VE_sup_zero_Moz <- NLOG_VE_sup_zero[NLOG_VE_sup_zero$Zone=="MOZ",]
-#North >10?S
-NLOG_VE_sup_zero_WIO <- NLOG_VE_sup_zero[NLOG_VE_sup_zero$Zone=="WIO",]
+if(NLOG_DATA_SOURCE == 'obs'){
+  
+  NLOG_VE_sup_zero <- NLOG_VE[NLOG_VE$NumNLOG>0,]
+  #Moz <10S
+  NLOG_VE_sup_zero_Moz <- NLOG_VE_sup_zero[NLOG_VE_sup_zero$Zone=="MOZ",]
+  #North >10?S
+  NLOG_VE_sup_zero_WIO <- NLOG_VE_sup_zero[NLOG_VE_sup_zero$Zone=="WIO",]
+  
+  write.csv(NLOG_VE_sup_zero, file.path(PATH_OUTPUT,"NLOG_VE_sup_zero.csv"), row.names = F)
+  write.csv(NLOG_VE_sup_zero_Moz, file.path(PATH_OUTPUT,"NLOG_VE_sup_zero_Moz.csv"), row.names = F)
+  write.csv(NLOG_VE_sup_zero_WIO, file.path(PATH_OUTPUT,"NLOG_VE_sup_zero_North.csv"), row.names = F)
+  
+  #### Absence NLOG
+  
+  NLOG_VE_zero <- NLOG_VE[NLOG_VE$NumNLOG==0,]
+  #Moz <10S
+  NLOG_VE_zero_Moz <- NLOG_VE_zero[NLOG_VE_zero$Zone=="MOZ",]
+  #North >10?S
+  NLOG_VE_zero_North <- NLOG_VE_zero[NLOG_VE_zero$Zone=="WIO",]
+  
+  write.csv(NLOG_VE_zero, file.path(PATH_OUTPUT,"NLOG_VE_zero.csv"), row.names = F)
+  write.csv(NLOG_VE_zero_Moz, file.path(PATH_OUTPUT,"NLOG_VE_zero_Moz.csv"), row.names = F)
+  write.csv(NLOG_VE_zero_North, file.path(PATH_OUTPUT,"NLOG_VE_zero_North.csv"), row.names = F)
+}
 
-write.csv(NLOG_VE_sup_zero, file.path(PATH_OUTPUT,"NLOG_VE_sup_zero.csv"), row.names = F)
-write.csv(NLOG_VE_sup_zero_Moz, file.path(PATH_OUTPUT,"NLOG_VE_sup_zero_Moz.csv"), row.names = F)
-write.csv(NLOG_VE_sup_zero_WIO, file.path(PATH_OUTPUT,"NLOG_VE_sup_zero_North.csv"), row.names = F)
-
-#### Absence NLOG
-
-NLOG_VE_zero <- NLOG_VE[NLOG_VE$NumNLOG==0,]
-#Moz <10S
-NLOG_VE_zero_Moz <- NLOG_VE_zero[NLOG_VE_zero$Zone=="MOZ",]
-#North >10?S
-NLOG_VE_zero_North <- NLOG_VE_zero[NLOG_VE_zero$Zone=="WIO",]
-
-write.csv(NLOG_VE_zero, file.path(PATH_OUTPUT,"NLOG_VE_zero.csv"), row.names = F)
-write.csv(NLOG_VE_zero_Moz, file.path(PATH_OUTPUT,"NLOG_VE_zero_Moz.csv"), row.names = F)
-write.csv(NLOG_VE_zero_North, file.path(PATH_OUTPUT,"NLOG_VE_zero_North.csv"), row.names = F)
 
